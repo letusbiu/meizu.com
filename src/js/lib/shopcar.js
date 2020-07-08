7e3e40f1033ef3d4a8f6f72404baf1bd65473670
BiShengZhuang <2310012061@qq.com>
2020-07-08 17:58:19 +0800

    v1.0.4

--- src/js/lib/shopcar.js
+++ src/js/lib/shopcar.js
@@ -4,13 +4,14 @@ let pic = "/src/img/details-img/phone-adv-img/"
 
 define(['jquery', 'cookie'], function($, cookie) {
     return {
-        render: function() {
-            let shop = cookie.get('shop'); //   获取cookie数据
+        render: function(callback) {
+            let shop = cookie.get('shop'); //获取现有的商品cookie数据
 
             if (shop) {
                 shop = JSON.parse(shop);
+                //遍历并获取商品cookie里面所有的id  存为一个以，隔开的字符串  10003,10002,10001
                 let idlist = shop.map(elm => elm.id).join();
-                // console.log(idlist)
+
                 $.ajax({
                     type: "get",
                     url: `${baseUrl}/interface/shop.php`,
@@ -19,15 +20,11 @@ define(['jquery', 'cookie'], function($, cookie) {
                     },
                     dataType: "json",
                     success: function(res) {
-                        // console.log(res);
                         let tempstr = '';
-
                         res.forEach(elm => {
-                            // cookie中获取 于当前从数据库中遍历出的相同元素
+                            // cookie中获取 和当前从数据库中遍历出的id相同元素，用于获取商品的数量和单价
                             let arr = shop.filter(val => val.id == elm.id);
-
-                            // console.log(arr);
-
+                            //拼接元素字符串
                             tempstr += `
                             <div class="msg-line2">
                                 <div class="msg-wrap">
@@ -48,14 +45,10 @@ define(['jquery', 'cookie'], function($, cookie) {
                                 </div>
                                <span style="color:#e02b41">￥<span><span class="totalprice">${arr[0].price * arr[0].num}</span>
                                 <span><a href="javascript:;" class="delete" data-id="${arr[0].id}">删除</a></span>
-                            </div>
-                            `;
-                            // console.log(tempstr)
+                            </div>`;
                         });
+                        //将商品放入页面
                         $('.msg-line1').after(tempstr);
-
-
-                        // callback && callback();
 
                         //全选  全不选功能
                         $(".quanxuan").click(function() {
@@ -68,11 +61,12 @@ define(['jquery', 'cookie'], function($, cookie) {
                                 $(".quanxuan").prop("checked", false);
                             }
                         });
+                        //增加选择商品 更改总价功能
                         $('.cbx').on('click', function() {
                             //声明变量 存放总价
                             let zongjia = 0;
                             //点击所有复选框 都会触发事件
-                            //选中所有当前处于选中状态的的复选框 的父元素的父元素 里面的 单项价格 遍历相加
+                            //选中所有当前处于checked状态的的复选框 的父元素的父元素 里面的 单项总价格 遍历相加
                             $('.danxuan:checked').each((index, elm) => {
                                 zongjia += (+$(elm).parent().parent().find('.totalprice').text());
                             });
@@ -81,18 +75,20 @@ define(['jquery', 'cookie'], function($, cookie) {
                         });
                         //删除功能
                         $('.delete').on('click', function() {
+                            //在拼接字符串时，为每一个删除标签添加了一个id
                             //获取删除标签上面的自定义属性data-id的属性值  也就是商品对应的id
+                            //后面的数量加减按钮也是依赖于这个id
                             let iddd = this.dataset.id;
 
                             //获取页面当前的cookie， 并筛选出除了此项以外的数据（数组形式）
                             let shop = JSON.parse(cookie.get('shop'))
                             shop = shop.filter(val => {
-                                    return val.id !== iddd
-                                })
-                                //将筛选结果转为json字符串并重新设置cookie
+                                return val.id !== iddd
+                            });
+                            //将筛选结果转为json字符串并更新cookie
                             shop = JSON.stringify(shop);
-                            cookie.set('shop', shop, 1)
-                                //刷新页面  由于购物车内的商品是用cookie内的id去数据库请求数据 渲染出来的  所以刷新即可实现删除
+                            cookie.set('shop', shop, 1);
+                            //刷新页面  由于购物车内的商品是用cookie内的id去数据库请求数据 渲染出来的  所以刷新即可实现删除
                             location.reload()
                         });
                         //数量加减功能
@@ -101,20 +97,23 @@ define(['jquery', 'cookie'], function($, cookie) {
                                 $(this).parent().parent().find('.num').val(+$(this).parent().parent().find('.num').val() - 1);
                                 //获取 加给右侧删除标签上面的自定义id
                                 let idd = $(this).parent().parent().find('.delete')[0].dataset.id;
-                                // console.log(idd)
                                 let coo = cookie.get('shop');
-                                // console.log(coo)
-                                let newnum = +$(this).parent().parent().find('.num').val()
-                                console.log(newnum)
+                                //获取新的数量
+                                let newnum = +$(this).parent().parent().find('.num').val();
+                                //修改cookie存放的对应商品的数量
                                 coo = JSON.parse(coo)
                                 coo.map(function(val) {
                                     idd == val.id ? val.num = newnum : null;
-                                })
+                                });
+                                //更新cookie
                                 cookie.set('shop', JSON.stringify(coo), 1);
-                                let jiaqian = +$(this).parent().parent().find('.price').html().slice(1);
-                                console.log(jiaqian);
 
+                                //获取单价
+                                let jiaqian = +$(this).parent().parent().find('.price').html().slice(1);
+                                //修改单项总价
                                 $(this).parent().parent().find('.totalprice').html(newnum * jiaqian)
+
+
                                 let zongjia = 0;
                                 //点击所有复选框 都会触发事件
                                 //选中所有当前处于选中状态的的复选框 的父元素的父元素 里面的 单项价格 遍历相加
@@ -127,7 +126,7 @@ define(['jquery', 'cookie'], function($, cookie) {
                         })
 
                         $('.btnup').on('click', function() {
-                            //添加功能
+                            //原理同上
                             $(this).parent().parent().find('.num').val(+$(this).parent().parent().find('.num').val() + 1);
                             //获取 加给右侧删除标签上面的自定义id
                             let idd = $(this).parent().parent().find('.delete')[0].dataset.id;
@@ -159,10 +158,12 @@ define(['jquery', 'cookie'], function($, cookie) {
                         });
                         //退出功能
                         $('.exit').on('click', function() {
+                            //删除cookie  刷新页面 即可实现
                             cookie.remove('user');
                             location.href = "http://localhost/meizu.com/src/html/index.html"
-                        })
-
+                        });
+                        //调用回调，设置打开页面后 所有复选框都处于被选中的状态
+                        callback();
                     }
                 });
             }
--- src/js/main.shopcar.js
+++ src/js/main.shopcar.js
@@ -7,5 +7,15 @@ require.config({
 });
 
 require(['shopcar'], function(shopcar) {
-    shopcar.render();
+    shopcar.render(function() {
+        //将所有复选框全设置成checked状态
+        $('.cbx').prop('checked', true);
+        //修改总价
+        let zongjia = 0;
+        $('.danxuan:checked').each((index, elm) => {
+            zongjia += (+$(elm).parent().parent().find('.totalprice').text());
+        });
+        //放入相应的地方
+        $('.zongjia').text(zongjia)
+    });
 })
